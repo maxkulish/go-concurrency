6a3d35cf2e66e531fac83c379e2e9b01c27ccec3
Max Kulish <kma.memo@gmail.com>
2021-09-23 08:08:09 +0300

    or-done pattern

---
+++ ch04/or-done/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	orDone := func(done, c <-chan interface{}) <-chan interface{} {
+		valStream := make(chan interface{})
+		go func() {
+			defer close(valStream)
+			for {
+				select {
+				case <-done:
+					return
+				case v, ok := <-c:
+					if ok == false {
+						return
+					}
+					select {
+					case valStream <- v:
+					case <-done:
+					}
+				}
+			}
+		}()
+		return valStream
+	}
+
+	myChan := make(chan interface{})
+	done := make(chan interface{})
+	for val := range orDone(done, myChan) {
+		// Do something with val
+		fmt.Printf("%v", val)
+	}
+}
